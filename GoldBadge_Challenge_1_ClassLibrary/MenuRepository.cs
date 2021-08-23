7a59b2a02abdb3de38f3c3445253ca5f1a9cd745
Chris Crowley <cfcrowely42@gmail.com>
2021-08-23 16:22:37 -0400

    Added Repo class

--- GoldBadge_Challenge_1_ClassLibrary/MenuRepository.cs
+++ GoldBadge_Challenge_1_ClassLibrary/MenuRepository.cs
@@ -8,5 +8,23 @@ namespace GoldBadge_Challenge_1_ClassLibrary
 {
     public class MenuRepository
     {
+        private readonly List<MenuItem> _menuItem = new List<MenuItem>();
+
+
+        
+        public void AddToList(MenuItem food)
+        {
+            _menuItem.Add(food);
+        }
+
+        public List<MenuItem> GetOurMenu()
+        {
+            return _menuItem;
+        }
+
+        public void DeleteOurItems(MenuItem rancidFood)
+        {
+            _menuItem.Remove(rancidFood);
+        }
     }
 }
