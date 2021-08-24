fa1a5dc672419ed80e64a307678e97d7d010d098
Chris Crowley <cfcrowely42@gmail.com>
2021-08-24 16:37:19 -0400

    started challenge 2

---
+++ Challenge_2_Classes/Claim.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2_Classes
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType ClaimType { get; set; }
+        public string Description { get; set; }
+        public double DamageCost { get; set; }
+        public DateTimeOffset DateOfAccident { get; set; }
+        public DateTimeOffset DateOfClaim { get; set; }
+     
+        
+        public bool IsValid { get
+            {
+                TimeSpan timeFromAccident = DateOfClaim - DateOfAccident;
+                if(timeFromAccident.Days >= 30)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+
+    public enum ClaimType { Car, Home, Theft }
+}
---
+++ Challenge_2_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge_2_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge_2_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7bf5a20-7750-4605-ba3e-b52b31a08637")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
