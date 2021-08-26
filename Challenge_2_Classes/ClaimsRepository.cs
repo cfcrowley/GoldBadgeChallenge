56a89e1b875812bc065d863800ab7a489b75f613
Chris Crowley <cfcrowely42@gmail.com>
2021-08-26 12:42:18 -0400

    added update and getbydescription functions to challenge 2

--- Challenge_2_Classes/ClaimsRepository.cs
+++ Challenge_2_Classes/ClaimsRepository.cs
@@ -29,7 +29,42 @@ namespace Challenge_2_Classes
             return allClaims;
         }
 
-        public void DeleteOurItems(Claim badClaim)
+        public Claim GetClaimByDescription(string description)
+        {
+            // Loop through the list
+            // If content item has correct title
+            //    return the item
+            // return null
+            foreach (Claim c in _claims)
+            {
+                if (c.Description == description)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        public bool UpdateExistingContent(string originalClaim, Claim claim)
+        {
+            Claim oldClaim = GetClaimByDescription(originalClaim);
+
+            if (oldClaim != null)
+            {
+                oldClaim.ClaimType = claim.ClaimType;
+                oldClaim.ClaimID = claim.ClaimID;
+                oldClaim.DamageCost = claim.DamageCost;
+                oldClaim.Description = claim.Description;
+                oldClaim.DateOfAccident = claim.DateOfAccident;
+                oldClaim.DateOfClaim = claim.DateOfClaim;
+
+                return true;
+            }
+
+            return false;
+        }
+
+public void DeleteOurItems(Claim badClaim)
         {
             _claims.Remove(badClaim);
         }
